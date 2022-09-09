7534555600235d9e5231c49912747d2f0b2da64a
xiaoyu <xiaoyucoder@163.com>
2022-09-09 17:05:58 +0800

    市场接口调整

--- web/src/api/marketplace.js
+++ web/src/api/marketplace.js
@@ -2,28 +2,28 @@ import request from '@/utils/request'
 
 export function list(data) {
   return request({
-    url: '/api/crud-hub/marketplace/list',
+    url: '/api/crud-admin/marketplace/list',
     method: 'post',
     data
   })
 }
 export function get(id) {
   return request({
-    url: '/api/crud-hub/marketplace/get',
+    url: '/api/crud-admin/marketplace/get',
     method: 'get',
     params: { id }
   })
 }
 export function getByToken(accessToken) {
   return request({
-    url: '/api/crud-hub/plugin/getTemplate',
+    url: '/api/crud-admin/plugin/getTemplate',
     method: 'get',
     params: { accessToken }
   })
 }
 export function getTablesByDdl(data) {
   return request({
-    url: '/api/crud-hub/plugin/getTablesByDdl',
+    url: '/api/crud-admin/plugin/getTablesByDdl',
     method: 'post',
     headers: { 'Content-Type': 'application/json;charset=UTF-8' },
     data
@@ -31,14 +31,14 @@ export function getTablesByDdl(data) {
 }
 export function generateProject(data) {
   return request({
-    url: '/api/crud-hub/plugin/generateProject',
+    url: '/api/crud-admin/plugin/generateProject',
     method: 'post',
     data
   })
 }
 export function generateCode(data) {
   return request({
-    url: '/api/crud-hub/plugin/generateCode',
+    url: '/api/crud-admin/plugin/generateCode',
     method: 'post',
     data
   })
